bb7d2ab2ef7f91a04e15fad1b3f980e0bb5fa61d
LCL <patrick881212@gmail.com>
2020-04-03 18:28:42 +0800

    third commit

--- generate.cpp
+++ generate.cpp
@@ -1,8 +1,7 @@
 #include<iostream>
 #include"sudoku.h"
 using namespace std;
-int main(void)
-{
+int main(void) {
     int arr[81] = {3, 0, 2, 0, 0, 5, 6, 9, 0,
                    0, 4, 0, 0, 9, 6, 0, 3, 0,
                    0, 5, 0, 0, 0, 8, 0, 0, 0,
@@ -11,17 +10,17 @@ int main(void)
                    5, 0, 7, 0, 3, 0, 0, 6, 1,
                    0, 0, 0, 8, 0, 0, 0, 2, 0,
                    0, 8, 0, 9, 6, 0, 0, 7, 0,
-                   0, 6, 5, 7, 0, 0, 3, 0, 9};
+                   0, 6, 5, 7, 0, 0, 3, 0, 9
+                  };
     Sudoku su(arr);
     su.rotate(2);
-    for (int i = 0; i < 81;i++)
-        {
-            
-            cout << su.getanswer(i);
-            if(i%9!=8)
-                cout << " ";
-            else
-                cout << endl;
-        }
+    for (int i = 0; i < 81; i++) {
+
+        cout << su.getanswer(i);
+        if(i%9!=8)
+            cout << " ";
+        else
+            cout << endl;
+    }
     return 0;
 }
--- transform.cpp
+++ transform.cpp
@@ -1,61 +1,52 @@
 #include<iostream>
 #include"sudoku.h"
 using namespace std;
-int main(void)
-{
+int main(void) {
     int arr[81];
     bool result = true;
-    for (int i = 0; i < 81;i++)
-            cin >> arr[i];
+    for (int i = 0; i < 81; i++)
+        cin >> arr[i];
     Sudoku su(arr);
-    while(result)
-    {
+    while(result) {
         int flag,x,y;
         cin >> flag;
-        switch(flag)
-        {
-            case 0:
-                result = false;
-                break;
-            case 1:
-            {
-                cin >> x >> y;
-                su.swapNum(x, y);
-            }
+        switch(flag) {
+        case 0:
+            result = false;
             break;
-            case 2:
-            {
-                cin >> x >> y;
-                su.swapRow(x, y);
-            }
-            break;
-            case 3:
-            {
-                cin >> x >> y;
-                su.swapCol(x, y);
-            }
-            break;
-            case 4:
-            {
-                cin >> x;
-                su.rotate(x);
-            }
-            break;
-            case 5:
-            {
-                cin >> x;
-                su.flip(x);
-            }
-            break;
+        case 1: {
+            cin >> x >> y;
+            su.swapNum(x, y);
+        }
+        break;
+        case 2: {
+            cin >> x >> y;
+            su.swapRow(x, y);
+        }
+        break;
+        case 3: {
+            cin >> x >> y;
+            su.swapCol(x, y);
+        }
+        break;
+        case 4: {
+            cin >> x;
+            su.rotate(x);
+        }
+        break;
+        case 5: {
+            cin >> x;
+            su.flip(x);
+        }
+        break;
         }
     }
-    for (int i = 0; i < 81;i++)
-        {
-            
-            cout << su.getanswer(i);
-            if(i%9!=8)
-                cout << " ";
-            else
-                cout << endl;
-        }
+    for (int i = 0; i < 81; i++) {
+
+        cout << su.getanswer(i);
+        if(i%9!=8)
+            cout << " ";
+        else
+            cout << endl;
+    }
 }
